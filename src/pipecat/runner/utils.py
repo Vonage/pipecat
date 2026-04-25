d9153989c252142600f86f010e8ec6daf500fbbb
asilvestre <antoni.silvestrepadros@vonage.com>
2026-04-25 08:34:07 +0200

    apply suggestions in PR

--- src/pipecat/runner/utils.py
+++ src/pipecat/runner/utils.py
@@ -572,7 +572,7 @@ async def create_transport(
                 vad_analyzer=SileroVADAnalyzer(),
                 # add_wav_header and serializer will be set automatically
             ),
-            "vonage": lambda: VonageVideoConnectorParams(
+            "vonage": lambda: VonageVideoConnectorTransportParams(
                 audio_in_enabled=True,
                 audio_out_enabled=True,
                 vad_analyzer=SileroVADAnalyzer(),
